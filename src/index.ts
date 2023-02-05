9664270c5e501825c76dc0389fade45784e52285
danacohen <danah2139|@|gmail.com>
2023-02-05 13:07:38 +0200

    add views

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,19 @@ sequelizeConnection.sync().then(() => {
   });
 });
 
+app.set("view engine", "pug");
+
+app.get("/", async (req, res) => {
+  const results = await sequelizeConnection.query(
+    "SELECT * FROM Orders as o join OrderLines ol on o.retailer_id = ol.variant_id"
+  );
+
+  console.log(results);
+
+  // Assuming `fields` contains the data you wish to use in your template
+  res.render("index.pug", { results });
+});
+
 async function queueBuild() {
   await OrderQueue.Instance.build();
 }
